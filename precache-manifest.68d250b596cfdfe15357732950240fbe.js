98b39566a5beb122b2ade15291294112913db4a9
阿修 <huilong494@gmail.com>
2019-10-26 12:05:29 -0700

    Updates

---
+++ precache-manifest.68d250b596cfdfe15357732950240fbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c9ecebec82bdbdd75c71f8f53d62cf",
+    "url": "/RobotCards/index.html"
+  },
+  {
+    "revision": "568495f0ffa175128f3a",
+    "url": "/RobotCards/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "837f73a171ea7555dc50",
+    "url": "/RobotCards/static/css/main.4b0a5a92.chunk.css"
+  },
+  {
+    "revision": "568495f0ffa175128f3a",
+    "url": "/RobotCards/static/js/2.996cb6d9.chunk.js"
+  },
+  {
+    "revision": "837f73a171ea7555dc50",
+    "url": "/RobotCards/static/js/main.d381e841.chunk.js"
+  },
+  {
+    "revision": "1a236aede491db7ebb16",
+    "url": "/RobotCards/static/js/runtime~main.079a8b82.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotCards/static/media/SEGA.12a3db8b.woff"
+  }
+]);
