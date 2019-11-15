060e750d45b001bb3c3e064af38567ad5b077ffe
huilong494 <anitachang@huilong494de-MacBook-Pro.local>
2019-11-14 18:40:49 -0800

    react-redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,55 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 //import robots from './robots'
+import { connect } from 'react-redux';
 import Scroll from '../components/Scroll';
 import SearchBox from '../components/SearchBox';
 import './App.css'
 import ErrorBoundry from '../components/ErrorBoundry';
+import { setSearchField, requestRobots } from '../actions'
 
+const mapStateToProps = state => {
+    //recieve a state
+    return{
+        searchField: state.searchRobots.searchField,
+        //in index.js we create a store as state with searchRobots reduxer
+        //reduxer裡的searchRobots
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+//recieve a dispatch which use to send action
+    return {
+        onSearchChange: (event) => dispatch(
+            // dispatch an action 
+            setSearchField(event.target.value)
+            //event.target.value === 'the word that user typed'
+        ),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
-
+    
     componentDidMount = () =>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then( response => response.json() )
-            .then( users => this.setState( {robots: users }))
+        this.props.onRequestRobots();
     }
-
+ //because we create the store
+ /*
     onSearchChange = (event) =>{ //去查為什麼要用onSearchChange = (event) =>
         //never do this.state.robots =  'abc'
         this.setState({ searchfield: event.target.value })
     }
+*/
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+        const { searchField, onSearchChange, robots, isPending } = this.props;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase());
         })
-        if (this.state.robots.length === 0 ) {
+        if (isPending) {
             return (
                 <div className='tc'>
                     <h1>RobotFriends</h1>
@@ -41,7 +60,7 @@ class App extends Component {
             return (
                 <div className='tc'>
                     <h1>RobotFriends</h1>
-                    <SearchBox searchChange={ this.onSearchChange }/>
+                    <SearchBox searchChange={ onSearchChange }/>
                     <Scroll>
                         <ErrorBoundry>
                             <CardList robots ={ filteredRobots }/>
@@ -53,4 +72,8 @@ class App extends Component {
     }
 
 }
-export default App;
+export default connect( mapStateToProps, mapDispatchToProps )(App);
+//high order component
+//connect function is a higher function that returns another function
+//so connect is going to run the App
+//connect give those props(mapStateToProps,mapDispatchToProps) to the Apps
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger'
 //import Hello from '../components/Hello';
 import App from './containers/App'; //asume it is '.js',if there's no '.css'
 import * as serviceWorker from './serviceWorker';
 //import CardList from '../components/CardList'
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
+import thunkMiddleware from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots,requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger)); 
+//It should only be one store in a project
+//You'll have lots of reducers in your application ,
+//but you have to create a root reducer, 
+//than use root reducer to create the store
+
+ReactDOM.render(
+    <Provider store= {store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 //ReactDOM.render(<Hello />, document.getElementById('root'));
 //ReactDOM.render(<CardList robots={robots}/>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
